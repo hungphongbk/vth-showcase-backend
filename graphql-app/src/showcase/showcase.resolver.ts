19ea4f26fe1d6617436216e5498c51bb8d522195
hungphongtruong <programmingd32@gmail.com>
2021-11-17 20:55:17 +0700

    enhance resolvers

--- graphql-app/src/showcase/showcase.resolver.ts
+++ graphql-app/src/showcase/showcase.resolver.ts
@@ -1,14 +1,10 @@
 import { Query, Resolver } from '@nestjs/graphql';
 import { ShowcaseService } from './showcase.service';
 import { ShowcaseModel } from './showcase.model';
-import { MediaService } from '../media/media.service';
 
 @Resolver(() => ShowcaseModel)
 export class ShowcaseResolver {
-  constructor(
-    private itemService: ShowcaseService,
-    private mediaService: MediaService,
-  ) {}
+  constructor(private itemService: ShowcaseService) {}
 
   @Query(() => [ShowcaseModel], { name: 'showcases' })
   async showcases(): Promise<ShowcaseModel[]> {
