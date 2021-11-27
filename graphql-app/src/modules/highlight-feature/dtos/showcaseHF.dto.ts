80dab8da782fb1072b3095caad1969b15af087d8
hungphongtruong <programmingd32@gmail.com>
2021-11-27 19:38:37 +0700

    remove circular

--- graphql-app/src/modules/highlight-feature/dtos/showcaseHF.dto.ts
+++ graphql-app/src/modules/highlight-feature/dtos/showcaseHF.dto.ts
@@ -1,6 +1,5 @@
 import { Field, ID, InputType, ObjectType } from '@nestjs/graphql';
 import { FilterableField, Relation } from '@nestjs-query/query-graphql';
-import { ShowcaseDto } from '../../showcase/dtos/showcase.dtos';
 import { MediaDto } from '../../media/dtos/media.dto';
 
 @ObjectType({ isAbstract: true })
@@ -15,7 +14,6 @@ export class ShowcaseHFBaseDto {
 
 @ObjectType('ShowcaseHighlightFeature')
 @Relation('image', () => MediaDto)
-@Relation('showcase', () => ShowcaseDto)
 export class ShowcaseHFDto extends ShowcaseHFBaseDto {
   @FilterableField(() => ID)
   id: string;
