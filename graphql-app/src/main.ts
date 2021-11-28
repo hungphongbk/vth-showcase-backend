3c28ef85cfa6961e6b29438012a17cf02656dd67
hungphongtruong <programmingd32@gmail.com>
2021-11-28 07:59:52 +0700

    switch to fastify

--- graphql-app/src/main.ts
+++ graphql-app/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as morgan from 'morgan';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
   app.use(morgan('tiny'));
   await app.listen(process.env.PORT ?? 3000);
 }
