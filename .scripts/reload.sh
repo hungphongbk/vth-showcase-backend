ca1993b836048100429a5649d93241a14fb22b66
hungphongtruong <programmingd32@gmail.com>
2021-11-29 12:01:49 +0700

    add validate guard

--- .scripts/reload.sh
+++ .scripts/reload.sh
@@ -1,78 +1,79 @@
 #!/bin/bash
 
-trap "exit 1" TERM
-export TOP_PID=$$
-
-originalDir=`pwd`
+original_dir=`pwd`
 cd "$(dirname "$0")"
 
-serviceName=$1
-nginxContainerName=${2:-nginx}
+service_name=$(echo $1|tr -d '\n')
+nginx_container_name=${2:-nginx}
 docker_compose_cmd='docker-compose -f ../docker-compose.prod.yml'
 
-exit_with_error(){
-  cd $originalDir
-  kill -s TERM $TOP_PID
-}
-
 reload_nginx() {
-  docker exec $nginxContainerName /usr/sbin/nginx -s reload
+  docker exec $nginx_container_name /usr/sbin/nginx -s reload &>/dev/null
 }
 
 server_status() {
   local port=$1
-  local status=$(curl -is --connect-timeout 5 --show-error http://localhost:$port | head -n 1 | cut -d " " -f2)
+  status=$(curl -is --connect-timeout 5 --show-error http://localhost:$port | head -n 1 | cut -d " " -f2)
 
   # if status is not a status code (123), means we got an error not an http header
   # this could be a timeout message, connection refused error msg, and so on...
   if [[ $(echo ${#status}) != 3 ]]; then
-    echo "503"
+    status="503"
   fi
-
-  echo $status
+  status="$status"
 }
 
 wait_for_available() {
   local port=$1
-  while [[ $(server_status $port) -gt "404" ]]; do
-    echo "New instance of $serviceName is getting ready..."
+  retry=0
+  server_status $port
+
+  while [[ $status -gt "404" ]]; do
+    if [[ $retry -gt 10 ]];then
+      break;
+      return 1
+    fi
+    ((retry++))
+    echo "New instance of $service_name:$port is getting ready..."
     sleep 2
+    server_status $port
   done
+  return 0
 }
 
-update_server(){
-  old_container_id=$(docker ps -f name=$serviceName -q | tail -n1)
-  ${docker_compose_cmd} up --build -d --no-deps --scale $serviceName=2 --no-recreate $serviceName
-  container_id=$(docker ps -f name="$serviceName" -q | tail -n1)
+#
+# SPINNING UP SERVER
+#
 
-  if [[ -z $container_id ]]; then
-    >&2 echo "$serviceName container not found, please double-check!"
-    exit_with_error
-  fi
-
-  container_port=$(docker port "$container_id" | cut -d " " -f3 | cut -d ":" -f2)
-  if [[ -z $container_port ]]; then
-    >&2 echo "Cannot found any port binds on $serviceName, please double-check!"
-    exit_with_error
-  fi
+old_container_id=$(docker ps -f name=$service_name -q | tail -n1)
+${docker_compose_cmd} up -d --no-build --no-deps --scale $service_name=2 --no-recreate $service_name
+container_id=$(docker ps -f name=$service_name -q | tail -n1)
 
 
-  export -f wait_for_available
-  timeout 30 bash -c wait_for_available
-  if [ $? -ne 0 ]; then
-    >&2 echo "New container has not been successfully deployed. Roll back..."
-    docker rm $container_id -f
-    exit_with_error
-  fi
+if [[ -z $container_id ]]; then
+  >&2 echo "$service_name container not found, please double-check!"
+  exit 1
+fi
 
-  reload_nginx
+container_port=$(docker port "$container_id" | cut -d " " -f3 | cut -d ":" -f2)
+if [[ -z $container_port ]]; then
+  >&2 echo "Cannot found any port binds on $service_name, please double-check!"
+  exit 1
+fi
 
-  docker rm $old_container_id -f
+wait_for_available $container_port
+if [ $? -gt 0 ]; then
+  >&2 echo "New container has not been successfully deployed. Roll back..."
+  docker rm $container_id -f
+  exit 1
+fi
+echo "$service_name:$container_port is ready. Spinning up right now..."
 
-  reload_nginx
+reload_nginx
+echo "Remove old $service_name container..."
+docker rm $old_container_id -f &>/dev/null
+reload_nginx
 
-  echo "DONE !"
-}
+echo "DONE !"
 
-update_server
-cd $originalDir
+cd $original_dir
