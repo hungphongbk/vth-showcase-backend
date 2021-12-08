b67b66ecdb1eaf5a771aabe0762a7fce059ab8c0
hungphongtruong <programmingd32@gmail.com>
2021-12-08 15:41:31 +0700

    add cache

--- graphql-app/src/app.module.ts
+++ graphql-app/src/app.module.ts
@@ -1,6 +1,6 @@
 // noinspection PointlessArithmeticExpressionJS
 
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as connectionOptions from './ormconfig';
@@ -10,11 +10,30 @@ import firebaseConfig from './config/firebase.config';
 import { FirebaseAdminModule } from '@tfarras/nestjs-firebase-admin';
 import { AuthModule } from './auth/auth.module';
 import { DataModulesModule } from './data-modules/data-modules.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as redisStore from 'cache-manager-redis-store';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        REDIS_HOST: Joi.string().required(),
+        REDIS_PORT: Joi.number().required(),
+        // ...
+      }),
+    }),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        store: redisStore,
+        host: configService.get('REDIS_HOST'),
+        port: configService.get('REDIS_PORT'),
+        ttl: 120,
+      }),
+    }),
     FirebaseAdminModule.forRootAsync({
       useFactory: firebaseConfig,
     }),
@@ -31,6 +50,12 @@ import { ConfigModule } from '@nestjs/config';
     DataModulesModule,
     AuthModule,
   ],
-  providers: [GqlLoggingPlugin],
+  providers: [
+    GqlLoggingPlugin,
+    // {
+    //   provide: APP_INTERCEPTOR,
+    //   useClass: CacheInterceptor,
+    // },
+  ],
 })
 export class AppModule {}
