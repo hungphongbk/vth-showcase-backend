abd605269d088183738c7a0b3a5b95850a83f70a
hungphongtruong <programmingd32@gmail.com>
2021-12-13 17:49:46 +0700

    reduce build scripts

--- .scripts/local-sync-env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-original_dir=`pwd`
-cd "$(dirname "$0")/../"
-
-gh secret set SERVER_ENV --env=preview < .env.staging
-gh secret set SERVER_ENV --env=production < .env.production
-
-cd $original_dir
