816669ad06fb260ca986359975d1ff94c2707f7a
hungphongtruong <programmingd32@gmail.com>
2022-01-14 09:38:02 +0700

    update schema

--- graphql-app/apps/main/src/data-modules/preorder/preorder.module.ts
+++ graphql-app/apps/main/src/data-modules/preorder/preorder.module.ts
@@ -6,6 +6,7 @@ import { PreorderEntity } from './entities/preorder.entity';
 import { PreorderResolver } from './preorder.resolver';
 import { ShowcaseModule } from '../showcase/showcase.module';
 import { PreorderGraphqlModule } from './modules/preorder.graphql.module';
+import { PreorderUserResolver } from './preorder.user.resolver';
 
 const authoredModule = AuthModule.forFeature({
   imports: [PreorderGraphqlOrmModule],
@@ -20,7 +21,7 @@ const authoredModule = AuthModule.forFeature({
     authoredModule,
     ShowcaseModule,
   ],
-  providers: [PreorderResolver],
+  providers: [PreorderResolver, PreorderUserResolver],
   exports: [PreorderGraphqlOrmModule],
 })
 export class PreorderModule {}
