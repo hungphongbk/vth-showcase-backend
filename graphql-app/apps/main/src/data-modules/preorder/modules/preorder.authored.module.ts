e7aa23a7357ab4d6aad33d5b335f9d30d99e1179
hungphongtruong <programmingd32@gmail.com>
2022-01-22 15:55:17 +0700

    fix preorder listing

---
+++ graphql-app/apps/main/src/data-modules/preorder/modules/preorder.authored.module.ts
@@ -0,0 +1,8 @@
+import { AuthModule } from '../../../auth';
+import { PreorderEntity } from '../entities/preorder.entity';
+import { PreorderGraphqlOrmModule } from './preorder.graphql-orm.module';
+
+export const PreorderAuthoredModule = AuthModule.forFeature({
+  imports: [PreorderGraphqlOrmModule],
+  EntityClass: PreorderEntity,
+});
--- graphql-app/apps/main/src/data-modules/preorder/modules/preorder.graphql.module.ts
+++ graphql-app/apps/main/src/data-modules/preorder/modules/preorder.graphql.module.ts
@@ -2,13 +2,17 @@ import { NestjsQueryGraphQLModule } from '@nestjs-query/query-graphql';
 import { PreorderDto } from '../dtos/preorder.dto';
 import { PreorderEntity } from '../entities/preorder.entity';
 import { PreorderGraphqlOrmModule } from './preorder.graphql-orm.module';
+import { PreorderAuthoredModule } from './preorder.authored.module';
+import { PreorderQueryService } from '../preorder-query-service';
 
 export const PreorderGraphqlModule = NestjsQueryGraphQLModule.forFeature({
-  imports: [PreorderGraphqlOrmModule],
+  imports: [PreorderGraphqlOrmModule, PreorderAuthoredModule],
+  services: [PreorderQueryService],
   resolvers: [
     {
       DTOClass: PreorderDto,
       EntityClass: PreorderEntity,
+      ServiceClass: PreorderQueryService,
       // read: { one: { disabled: true }, many: { disabled: true } },
       create: { one: { disabled: true }, many: { disabled: true } },
       update: { one: { disabled: true }, many: { disabled: true } },
