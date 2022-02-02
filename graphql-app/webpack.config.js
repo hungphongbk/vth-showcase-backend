16e07ddf2d0d5c3fbce13de55169db2523ae68ca
hungphongtruong <programmingd32@gmail.com>
2022-02-02 18:01:26 +0700

    add sentry source maps

--- graphql-app/webpack.config.js
+++ graphql-app/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = function (options) {
         // sentry-cli configuration - can also be done directly through sentry-cli
         // see https://docs.sentry.io/product/cli/configuration/ for details
         authToken: process.env.SENTRY_AUTH_TOKEN,
-        organization: 'vaithuhaycom',
+        org: 'vaithuhaycom',
         project: 'showcase-backend',
         release: '1.0',
 
