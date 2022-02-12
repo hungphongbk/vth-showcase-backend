838125712d5750ecb72d6c29c83565acb64e75ae
hungphongtruong <programmingd32@gmail.com>
2022-02-12 17:51:47 +0700

    add sentry to go upload app

--- upload-app/go.mod
+++ upload-app/go.mod
@@ -11,6 +11,7 @@ require (
 )
 
 require (
+	github.com/getsentry/sentry-go v0.12.0 // indirect
 	github.com/gin-contrib/cors v1.3.1 // indirect
 	github.com/go-pg/zerochecker v0.2.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
@@ -32,11 +33,11 @@ require (
 	github.com/golang/protobuf v1.4.3 // indirect
 	github.com/json-iterator/go v1.1.9 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
-	github.com/mattn/go-isatty v0.0.12 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
-	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
-	golang.org/x/sys v0.0.0-20210923061019-b8560ed6a9b7 // indirect
+	golang.org/x/sys v0.0.0-20211007075335-d3039528d8ac // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
--- upload-app/main/main.go
+++ upload-app/main/main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"fmt"
+	"github.com/getsentry/sentry-go"
 	"github.com/gin-contrib/cors"
 	"github.com/hungphongbk/vth-showcase-backend/main/db"
 	"github.com/hungphongbk/vth-showcase-backend/main/env"
 	"github.com/hungphongbk/vth-showcase-backend/main/repository"
 	"log"
 	"net/http"
+	"time"
 
 	healthcheck "github.com/RaMin0/gin-health-check"
+	sentrygin "github.com/getsentry/sentry-go/gin"
 	"github.com/gin-gonic/gin"
 )
 
@@ -27,6 +30,7 @@ func setupRouter() *gin.Engine {
 	r.Use(gin.Logger())
 	r.Use(healthcheck.Default())
 	r.Use(cors.Default())
+	r.Use(sentrygin.New(sentrygin.Options{}))
 
 	// Recovery middleware recovers from any panics and writes a 500 if there was one.
 	r.Use(gin.CustomRecovery(func(c *gin.Context, recovered interface{}) {
@@ -46,8 +50,20 @@ func setupRouter() *gin.Engine {
 }
 
 func main() {
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn:              "https://ff04c36e74dd4854afa0e8db643771c7@o164087.ingest.sentry.io/6200993",
+		TracesSampleRate: 0.5,
+	})
+	if err != nil {
+		log.Fatalf("sentry.Init: %s", err)
+	}
+	// Flush buffered events before the program terminates.
+	defer sentry.Flush(2 * time.Second)
+
+	sentry.CaptureMessage("It works!")
+
 	r := setupRouter()
-	err := r.Run(":25478")
+	err = r.Run(":25478")
 	if err != nil {
 		panic(err)
 	} else {
