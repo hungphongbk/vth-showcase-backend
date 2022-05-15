58800a3c3d6b54621551b076e0e1a45e282542d8
Phong Truong Hung <104372894+asombredance1218@users.noreply.github.com>
2022-05-15 18:54:47 +0700

    fix build errors

--- graphql-app/apps/main/src/migrations/1652199211297-add-showcase-invest-rel.ts
+++ graphql-app/apps/main/src/migrations/1652199211297-add-showcase-invest-rel.ts
@@ -22,6 +22,7 @@ export class addShowcaseInvestRel1652199211297 implements MigrationInterface {
             ALTER TABLE "showcase_invest_pkg_entity"
             ADD CONSTRAINT "FK_946065c8038bb2f728c555296e2" FOREIGN KEY ("showcaseId") REFERENCES "showcase"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
         `);
+    await queryRunner.query(`DROP VIEW IF EXISTS "showcase_view_entity"`);
     await queryRunner.query(`
             CREATE VIEW "showcase_view_entity" AS
             SELECT *,
@@ -37,37 +38,9 @@ export class addShowcaseInvestRel1652199211297 implements MigrationInterface {
                 )
             FROM "showcase" "showcase"
         `);
-    await queryRunner.query(
-      `
-            INSERT INTO "typeorm_metadata"(
-                    "database",
-                    "schema",
-                    "table",
-                    "type",
-                    "name",
-                    "value"
-                )
-            VALUES (DEFAULT, $1, DEFAULT, $2, $3, $4)
-        `,
-      [
-        'public',
-        'VIEW',
-        'showcase_view_entity',
-        'SELECT *, (SELECT count("comment"."id") AS "commentCount" FROM "comment" "comment" WHERE "comment"."showcaseId" = "showcase"."id") AS "commentCount", (SELECT count("preorder"."id") AS "preorderCount" FROM "preorder" "preorder" WHERE "preorder"."showcaseId" = "showcase"."id") FROM "showcase" "showcase"',
-      ],
-    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `
-            DELETE FROM "typeorm_metadata"
-            WHERE "type" = $1
-                AND "name" = $2
-                AND "schema" = $3
-        `,
-      ['VIEW', 'showcase_view_entity', 'public'],
-    );
     await queryRunner.query(`
             DROP VIEW "showcase_view_entity"
         `);
